e3a1dfe6adc98dfe731f1bb7f0ad8bdc5c84b110
LuizGuerra <luizpedrofg@gmail.com>
2021-04-03 17:08:28 -0300

    psvm input number

--- GameClient.java
+++ GameClient.java
@@ -2,6 +2,10 @@ import java.rmi.Naming;
 
 public class GameClient {
     public static void main(String[] args) {
+		if	(args.length != 1)  {
+			System.out.println("Uso: java GameClient <maquina>");
+			System.exit(1);
+		}
         try {
             ServerInterface helloInterface = (HelloInterface) Naming.lookup("//localhost/Hello");
             System.out.println(helloInterface.say());
--- GameServer.java
+++ GameServer.java
@@ -5,6 +5,10 @@ public class GameServer {
     static final int PORT = 1099;
     static final String ROOT_URL = "GameServer"
     public static void main (String[] args) {
+		if	(args.length != 1)  {
+			System.out.println("Uso: java GameServer <numero de jogadores>");
+			System.exit(1);
+		}
         try {
             java.rmi.registry.LocateRegistry.createRegistry(PORT);
             System.out.println("RMI registry ready");
